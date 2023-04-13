117c0d7545bb91e4febd4b245aefd9474d37bd1a
Zachary Robinson <zrobins2@swarthmore.edu>
2023-04-13 12:26:46 -0400

    feat: add GET route for dishes

---
+++ src/pages/api/dishes/index.ts
@@ -0,0 +1,47 @@
+import DishModel from '@/models/dish.model';
+import dbConnect from '@/util/mongodb';
+import { ToBoolean } from '@/util/toBoolean';
+import { Transform } from 'class-transformer';
+import { IsArray, IsOptional } from 'class-validator';
+import { createHandler, Get, Query, ValidationPipe } from 'next-api-decorators';
+
+/**
+ * Query parameters for filtering responses
+ */
+export class DishSearchRequest {
+  @IsOptional()
+  @ToBoolean()
+  isOrderable?: boolean;
+
+  @IsOptional()
+  @ToBoolean()
+  isArchived?: boolean;
+
+  @IsArray()
+  @Transform(({ value }) => value.trim().split(','))
+  categories?: string[] | { $in: string[] } = []; // typing is weird so we can turn this into a mongo $in query
+}
+
+class DishesHandler {
+  @Get()
+  async exportDishes(@Query(ValidationPipe) query: DishSearchRequest) {
+    dbConnect();
+
+    const dishes = DishModel;
+
+    if ((query.categories as string[]).length) {
+      // is not empty
+      query.categories = {
+        $in: query.categories as string[], // this 'as string[]' is safe because validation has ensured it
+      };
+    } else {
+      delete query.categories; // otherwise we don't return dishes that have categories set
+    }
+
+    console.log(query);
+
+    return await dishes.find(query, { options: false, dependencies: false }).exec();
+  }
+}
+
+export default createHandler(DishesHandler);
