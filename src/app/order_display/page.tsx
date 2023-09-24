1cfc129b9c6467b0e270501a805b3b007f2ae8d3
mountaint0p <summit100@gmail.com>
2023-09-24 16:34:16 -0400

    Added color and border rounding to order cards

--- src/app/order_display/page.tsx
+++ src/app/order_display/page.tsx
@@ -1,58 +1,71 @@
-import { Box, Card, CardHeader, Container, Typography, useTheme} from '@mui/material';
+import { Box, Card, CardHeader, Container, Typography, useTheme } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import CardContent from '@mui/material/CardContent';
 import styles from '../page.module.css';
 
-const stuffs : {order_number: string, name: string}[] = [
-  {order_number: "1",
-    name: "Tina Chen",},
-  {order_number: "1",
-    name: "Helen",
-  },
-  {order_number: "2",
-    name: "Summit",
-  }
-]
+const stuffs: { order_number: string; name: string }[] = [
+  { order_number: '1', name: 'Tina Chen' },
+  { order_number: '2', name: 'Helen Yin' },
+  { order_number: '3', name: 'Summit' },
+];
 
-function StuffCard({order_number, name} : 
-  {order_number : string, name : string}) {
+// testing for lot of orders: stuffs repeated 10 times
+const lotOfStuffs: { order_number: string; name: string }[] = Array.from({ length: 10 }, () => [
+  ...stuffs,
+]).flat();
 
-  return (  
-  <Card variant="outlined" sx={{ m: 2, p: 1 }}>
-    <CardContent>
-      <Box bgcolor={"red"} color={"white"}>
-         <Typography variant="h4" textAlign={"center"}> {name} </Typography>
-      </Box>
-      <Typography variant="h4" textAlign={"center"}> {order_number} </Typography>
-     
-    </CardContent>
-  </Card>
-  )
-};
+function StuffCard({ order_number, name }: { order_number: string; name: string }) {
+  return (
+    <Card sx={{ m: 2, p: 0, borderRadius: '16px', borderColor: 'black', border: 1 }}>
+      <Typography
+        variant="h5"
+        bgcolor={'green'}
+        color={'white'}
+        sx={{ width: '100%', p: 0, m: 0, borderStartEndRadius: '16px' }}
+        textAlign={'center'}
+      >
+        {' '}
+        {name}{' '}
+      </Typography>
+      <CardContent>
+        <Typography variant="h5" textAlign={'center'}>
+          {' '}
+          {order_number}{' '}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
 
 export default function Home() {
   return (
     <Container>
       <Box>
         <Card elevation={2} sx={{ m: 2, p: 2, backgroundColor: '#31425f' }}>
-          <Typography textAlign="center" variant="h3" color='white'>Crumb Cafe Orders</Typography>
+          <Typography textAlign="center" variant="h3" color="white">
+            Crumb Cafe Orders
+          </Typography>
         </Card>
         <Grid container spacing={2}>
           <Grid xs={6}>
             <Card sx={{ m: 2, p: 1 }}>
-              <Typography variant="h4" textAlign="center">Completed</Typography>
+              <Typography variant="h4" textAlign="center">
+                Completed
+              </Typography>
             </Card>
-          </Grid> 
+          </Grid>
           <Grid xs={6}>
             <Card sx={{ m: 2, p: 1 }}>
-              <Typography variant="h4" textAlign="center">Pending...</Typography>
+              <Typography variant="h4" textAlign="center">
+                Pending...
+              </Typography>
             </Card>
-          </Grid> 
-          {stuffs.map((stuff) =>
+          </Grid>
+          {lotOfStuffs.map((stuff) => (
             <Grid xs={3}>
-              <StuffCard name = {stuff.name} order_number = {stuff.order_number} />
+              <StuffCard name={stuff.name} order_number={stuff.order_number} />
             </Grid>
-          )}
+          ))}
         </Grid>
       </Box>
     </Container>
