0f2f7f27b83416cabf5101ed140d3acbc9d37e5a
Alina Vykliuk <alinavykliuk@Alinas-MacBook-Pro.local>
2023-09-30 17:29:35 -0400

    Buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import { Box, Card, Container, Typography } from '@mui/material';
+import { Box, Card, Container, Button, Typography } from '@mui/material';
 
 export default function Home() {
   return (
     <Container>
       <Box>
-        <Card elevation={2} sx={{ m: 2, p: 4 }}>
-          <Typography variant="h2">Hello World ~</Typography>
-          <Typography variant="body1">Here&apos;s a landing page for the Crumb Cafe.</Typography>
+        <Card elevation={2} sx={{ m: 10, p: 10 }}>
+          <Typography variant="h3">Welcome!</Typography>
+          <Typography variant="h2" style={{ fontWeight: 'bold' }}>The Crumb Cafe Homepage</Typography>
+
+          <Button variant="contained" size="large" sx={{ m: 1, p: 2, width: '130px' }}>Admin Page</Button>
+          <Button variant="contained" size="large" sx={{ m: 1, p: 2, width: '130px' }}>POS</Button>
+          <Button variant="contained" size="large" sx={{ m: 1, p: 2, width: '130px' }}>Kitchen</Button>
+          
         </Card>
       </Box>
     </Container>
