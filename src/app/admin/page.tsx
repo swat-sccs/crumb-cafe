1377bf36c26c267a42fd8422c123494c982f5918
Yue Zhang <yuezhang@Yues-MacBook-Air.local>
2023-10-01 15:20:57 -0400

    new layout for admin/dash

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,14 @@
+import { Box, Card, Container, Typography } from '@mui/material';
+
+export default function Home() {
+  return (
+    <Container>
+      <Box>
+        <Card elevation={2} sx={{ m: 2, p: 4 }}>
+          <Typography variant="h2">Hello World ~</Typography>
+          <Typography variant="body1">Here&apos;s a landing page for the Crumb Cafe.</Typography>
+        </Card>
+      </Box>
+    </Container>
+  );
+}
