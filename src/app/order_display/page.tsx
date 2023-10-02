6803e3229f8e824248a07f0c7ddf57d7d0d23110
Tina <tinaachenn14@gmail.com>
2023-10-01 23:16:45 -0400

    Update Order Display

--- src/app/order_display/page.tsx
+++ src/app/order_display/page.tsx
@@ -1,41 +1,86 @@
 import { Box, Card, CardHeader, Container, Typography, useTheme } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import CardContent from '@mui/material/CardContent';
 import styles from '../page.module.css';
 
-const stuffs: { order_number: string; name: string }[] = [
-  { order_number: '1', name: 'Tina Chen' },
-  { order_number: '2', name: 'Helen Yin' },
-  { order_number: '3', name: 'Summit' },
-];
 
-// testing for lot of orders: stuffs repeated 10 times
-const lotOfStuffs: { order_number: string; name: string }[] = Array.from({ length: 10 }, () => [
-  ...stuffs,
-]).flat();
+//stuff card functions that we will need for the kitchen order display
+// const stuffs: { order_number: string; name: string }[] = [
+//   { order_number: '1', name: 'Tina Chen' },
+//   { order_number: '2', name: 'Helen Yin' },
+//   { order_number: '3', name: 'Summit' },
+// ];
 
-function StuffCard({ order_number, name }: { order_number: string; name: string }) {
-  return (
-    <Card sx={{ m: 2, borderRadius: '16px', borderColor: 'black', border: 1 }}>
-      <Typography
-        variant="h5"
-        bgcolor={'green'}
-        color={'white'}
-        sx={{ width: '100%', borderStartEndRadius: '16px' }}
-        textAlign={'center'}
-      >
-        {' '}
-        {name}{' '}
-      </Typography>
-      <CardContent>
-        <Typography variant="h5" textAlign={'center'}>
-          {' '}
-          {order_number}{' '}
-        </Typography>
-      </CardContent>
-    </Card>
-  );
-}
+// // testing for lot of orders: stuffs repeated 10 times
+// const lotOfStuffs: { order_number: string; name: string }[] = Array.from({ length: 10 }, () => [
+//   ...stuffs,
+// ]).flat();
+
+// function StuffCard({ order_number, name }: { order_number: string; name: string }) {
+//   return (
+//     <Card sx={{ m: 2, borderRadius: '16px', borderColor: 'black', border: 1 }}>
+//       <Typography
+//         variant="h5"
+//         bgcolor={'green'}
+//         color={'white'}
+//         sx={{ width: '100%', borderStartEndRadius: '16px' }}
+//         textAlign={'center'}
+//       >
+//         {' '}
+//         {name}{' '}
+//       </Typography>
+//       <CardContent>
+//         <Typography variant="h5" textAlign={'center'}>
+//           {' '}
+//           {order_number}{' '}
+//         </Typography>
+//       </CardContent>
+//     </Card>
+//   );
+// }
+
+//if we want to use a table to display orders that are ready:
+// function createData(
+//   order_number: number,
+//   name: string,
+// ) {
+//   return { order_number, name };
+// }
+
+// const rows = [
+//   createData( 2, 'Summit'),
+//   createData( 3, 'Helen'),
+//   createData( 5, 'Tina'),
+// ];
+
+// function BasicTable() {
+//   return (
+//     <TableContainer component={Paper}>
+//       <Table sx={{ minWidth: 650 }} aria-label="simple table">
+//         <TableHead>
+//           <TableRow>
+//             <TableCell>Order Number</TableCell>
+//             <TableCell>Name</TableCell>
+//           </TableRow>
+//         </TableHead>
+//         <TableBody>
+//           {rows.map((row) => (
+//             <TableRow
+//               key={row.name}
+//               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+//             >
+//               <TableCell component="th" scope="row">
+//                 {row.order_number}
+//               </TableCell>
+//               <TableCell>{row.name}</TableCell>
+//             </TableRow>
+//           ))}
+//         </TableBody>
+//       </Table>
+//     </TableContainer>
+//   );
+// }
 
 export default function Home() {
   return (
@@ -47,33 +92,36 @@ export default function Home() {
           </Typography>
         </Card>
         <Grid container spacing={2}>
-          <Grid xs={6}>
+          <Grid xs={12}>
             <Card sx={{ m: 2, p: 1 }}>
               <Typography variant="h4" textAlign="center">
-                Completed
+                Ready
               </Typography>
             </Card>
           </Grid>
-          <Grid xs={6}>
+          <Card elevation={2} sx={{ m: 2, p: 2, width: 1200, height:450 }}>
+          <Typography textAlign="left" variant="h3">
+            Order #1: Tina
+          </Typography>
+        </Card>
+          {/* <Grid xs={12}>
+            <BasicTable />
+          </Grid> */}
+          {/* <Grid xs={6}>
             <Card sx={{ m: 2, p: 1 }}>
               <Typography variant="h4" textAlign="center">
                 Pending...
               </Typography>
             </Card>
-          </Grid>
-          {lotOfStuffs.map((stuff) => (
+          </Grid> */}
+          {/* {lotOfStuffs.map((stuff) => (
             <Grid xs={3}>
               <StuffCard name={stuff.name} order_number={stuff.order_number} />
             </Grid>
-          ))}
+          ))} */}
         </Grid>
       </Box>
     </Container>
   );
 
 }
-
-// what to do with long names & too many orders (makes page have to scroll)
-//how to change border color of each order name tag
-//split page in half for completed & pending orders
-// How to implement card content function for less code
