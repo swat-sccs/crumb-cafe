1c8bf43e19f4294d5d8f176592c7e9bd1205e43d
Yana <yayaorigami@Yanas-MacBook-Pro-2.local>
2023-10-29 15:14:25 -0400

    post request logic

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,8 +1,16 @@
 import dbConnect from '@/app/lib/mongodb';
 import OrderModel from '@/models/order.model';
+
+
+import DishModel from '@/models/dish.model';
+
+
+
 import { NextRequest, NextResponse } from 'next/server';
 import { z } from 'zod';
 import { parseQuery } from '../parseQuery';
+
+let customerNumberA = 1;
 
 const orderQuerySchema = z.object({
   status: z.enum(['new', 'in_progress', 'completed']).optional(),
@@ -11,6 +19,14 @@ const orderQuerySchema = z.object({
   createdAfter: z.coerce.date().optional(),
   updatedBefore: z.coerce.date().optional(),
   updatedAfter: z.coerce.date().optional(),
+});
+
+const newOrderSchema = z.object({ 
+  customerName: z.string(), //not optional
+  dish: z.string(),
+  options : z.array(z.string()).optional(),
+  notes : z.string().optional(),
+  //customDishOptions: OrderModel.CustomDishOptionsSchema().optional,
 });
 
 export async function GET(request: NextRequest) {
@@ -53,4 +69,42 @@ export async function GET(request: NextRequest) {
   return NextResponse.json({
     orders: res,
   });
+}
+
+
+export async function POST(request: NextRequest) {
+  //assign unique ID, just going up
+  const currentCustomerNumber = customerNumberA;
+  const data = { ...request.json(), status: "new", customerNumber: currentCustomerNumber };
+  customerNumberA = customerNumberA + 1;
+
+  const parsed  = newOrderSchema.safeParse(data); 
+
+  let parsedData;
+  if (parsed.success){
+    parsedData = parsed.data;
+  }else{
+    return new NextResponse("Poor input", {status: 400})
+  }
+
+  //double check that 1) the dish exists, and 2) isOrderable 
+    
+  try {
+    const dishValid = DishModel.findById(parsedData.dish);
+    //if (dishValid.isOrderable) ???
+  } catch (e) {
+    // probably a cast error because an invalid ID was supplied
+    return new NextResponse('Dish not found', { status: 404 });
+  }
+
+  await dbConnect();
+  
+  const newOrder = new OrderModel(parsedData);
+  const savedOrder = await newOrder.save();
+
+  return NextResponse.json({
+    order: savedOrder,
+  });  
+
+  
 }
