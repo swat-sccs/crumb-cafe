8bfb7e6fb8936eb4df936de0d71e6520642f9be8
Phillip Dinh <phillipdinh04@gmail.com>
2023-10-29 17:04:51 -0400

    scratch page has fullcalendar in it

---
+++ src/app/admin/scratch/PD/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { Grid } from "@mui/material";
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+
+export default function personalPD(){
+    return (
+        <FullCalendar
+            plugins={[ dayGridPlugin ]}
+            initialView="dayGridMonth"
+            weekends={false}
+            events={[
+                { title: 'taco tuesday', date: '2023-10-30' },
+                { title: 'first event!!', date: '2023-10-30' }
+            ]}
+        />
+    );
+
+}
