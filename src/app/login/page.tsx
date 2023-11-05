22242d1d8e609d6280ec73a0645dcde6d23f610b
Alina Vykliuk <alinavykliuk@dhcp-130-58-97-174.swarthmore.edu>
2023-11-05 15:38:50 -0500

    text fields

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,30 +1,34 @@
 'use client';
-import { Box, Card, Container, Button, Typography, Grid } from '@mui/material';
+import { TextField, Box, Card, Container, Button, Typography, Grid } from '@mui/material';
 
 export default function Home() {
   return (
-  <div>
-  <Card>
-    <Typography
-        sx={{marginTop: '5%',
-            textAlign: 'center'}}
-        variant="h2"
-        
-        style={{fontWeight: 'bold', color: 'white'}}
-    >
-    Welcome to Crumb!
-    </Typography>
-  </Card>
-  
-  <Button
-  sx={{ m: 10, p: 15}}>
+    <Grid container direction="column" justifyContent="center" alignItems="center">
+      <Card>
+        <Typography
+          sx={{ marginTop: '5%', textAlign: 'center' }}
+          variant="h2"
+          style={{ fontWeight: 'bold', color: 'white' }}
+        >
+          Welcome to Crumb!
+        </Typography>
+      </Card>
 
-    <Typography
-        variant="h2">
-        Sign in
-    </Typography>
-  </Button>
-  </div>
+      <Grid
+        alignItems="center"
+        justifyContent="space-evenly"
+        direction="column"
+        container
+        mt={5}
+        item
+        sx={{ backgroundColor: '', width: '100vw', height: '30vh' }}
+      >
+        <TextField id="outlined-basic" label="Username" variant="outlined">
+          {' '}
+        </TextField>
+
+        <TextField id="outlined-basic" label="Password" variant="outlined" type="password" />
+      </Grid>
+    </Grid>
   );
 }
-
