671efd2b2058230faea91db8a0f93d87f59baf7d
Alina Vykliuk <alinavykliuk@dhcp-130-58-103-76.swarthmore.edu>
2023-11-12 14:14:45 -0500

    Add textfields and buttons

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,30 +4,38 @@ import { TextField, Box, Card, Container, Button, Typography, Grid } from '@mui/
 export default function Home() {
   return (
     <Grid container direction="column" justifyContent="center" alignItems="center">
-      <Card>
-        <Typography
-          sx={{ marginTop: '5%', textAlign: 'center' }}
-          variant="h2"
-          style={{ fontWeight: 'bold', color: 'white' }}
-        >
-          Welcome to Crumb!
-        </Typography>
-      </Card>
+      <Typography
+        sx={{ marginTop: '5%', textAlign: 'center' }}
+        variant="h2"
+        style={{ fontWeight: 'bold', color: 'white' }}
+      >
+        Welcome to Crumb!
+      </Typography>
 
       <Grid
-        alignItems="center"
-        justifyContent="space-evenly"
-        direction="column"
         container
+        direction="column"
+        alignItems="center"
         mt={5}
-        item
         sx={{ backgroundColor: '', width: '100vw', height: '30vh' }}
       >
-        <TextField id="outlined-basic" label="Username" variant="outlined">
-          {' '}
-        </TextField>
+        <Grid item>
+          <TextField id="outlined-basic" label="Username" variant="outlined" />
+        </Grid>
 
-        <TextField id="outlined-basic" label="Password" variant="outlined" type="password" />
+        <Grid item mt={2}>
+          <TextField id="outlined-basic" label="Password" variant="outlined" type="password" />
+        </Grid>
+
+        <Grid item mt={3}>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ width: '200px', height: '60px', fontWeight: 'bold' }}
+          >
+            Sign In
+          </Button>
+        </Grid>
       </Grid>
     </Grid>
   );
