345c2d922a95d676471707849d82f2d61825a9c7
Yana <youtkin1@swarthmore.edu>
2023-11-21 23:21:36 -0500

    dishes dishId route post and put

--- src/app/api/dishes/[dishId]/route.ts
+++ src/app/api/dishes/[dishId]/route.ts
@@ -21,6 +21,33 @@ export async function GET(_request: NextRequest, { params }: { params: { dishId:
   }
 }
 
+export async function POST(_request: NextRequest) {
+  await dbConnect();
+
+  try {
+    const dish = _request.json();
+    await DishModel.create(dish);
+    return new NextResponse('Dish created successfully', { status: 201 });
+  } catch (e) {
+    return new NextResponse('Poor request', { status: 400 });
+  }
+}
+
+export async function PUT(_request: NextRequest, { params }: { params: { dishId: string } }) {
+  await dbConnect();
+  const dish = _request.json();
+  try {
+    const updatedDish = await DishModel.findByIdAndUpdate(params.dishId, dish);
+
+    if (!updatedDish) {
+      return new NextResponse('Dish not found', { status: 404 });
+    }
+    return new NextResponse('Dish updated successfully', { status: 200 });
+  } catch (e) {
+    return new NextResponse('Unable to update dish', { status: 404 });
+  }
+}
+
 export async function DELETE(_request: NextRequest, { params }: { params: { dishId: string } }) {
   await dbConnect();
 
