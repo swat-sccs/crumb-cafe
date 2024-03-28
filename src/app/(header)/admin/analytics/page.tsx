babbfd0e9b598fb952a6c7d2d886fea1185ed7d4
Damian <37555910+DCRepublic@users.noreply.github.com>
2024-03-28 17:37:04 -0400

    Bars

--- src/app/(header)/admin/analytics/page.tsx
+++ src/app/(header)/admin/analytics/page.tsx
@@ -7,11 +7,15 @@ import BasicCard from './card.js';
 import LabelAvatar from '@/app/components/labelAvatar.js';
 import { BarChart } from '@mui/x-charts/BarChart';
 import { axisClasses } from '@mui/x-charts';
+import dayjs, { Dayjs } from 'dayjs';
 
 import { PieChart, pieArcLabelClasses, pieArcClasses } from '@mui/x-charts/PieChart';
 import { useTheme } from '@mui/material/styles';
 import React, { useState, useEffect, useRef } from 'react';
 import moment from 'moment';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 import useSWR from 'swr';
 
@@ -25,6 +29,7 @@ export default function Analytics() {
   const [dailySales, setDailySales] = React.useState(0);
   const [totalOrders, setTotalOrders] = React.useState(0);
   const [dailyOrders, setDailyOrders] = React.useState(0);
+  const [currentDaySelected, setCurrentDay] = React.useState<Dayjs | null>(dayjs());
 
   const [sales, setSales] = React.useState([]);
   //const [data2, setData2] = React.useState<any>([]);
@@ -39,7 +44,7 @@ export default function Analytics() {
 
       for (const order of data.orders) {
         //(order);
-        if (moment().isSame(order.createdAt, 'day')) {
+        if (dayjs(currentDaySelected).isSame(dayjs(order.createdAt), 'day')) {
           tempDaily += order.total;
           tempDailyOrders++;
         }
@@ -59,7 +64,7 @@ export default function Analytics() {
     if (isLoading == false && !error) {
       data2 = [];
       let filteredOrders = data.orders.filter((a: any) => {
-        return moment().isSame(moment(a.createdAt), 'd');
+        return dayjs(currentDaySelected).isSame(dayjs(a.createdAt), 'day');
       });
 
       //console.log(data.orders);
@@ -98,6 +103,16 @@ export default function Analytics() {
 
   return (
     <Container>
+      <Box sx={{ position: 'absolute', top: 10, right: '30%' }}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            formatDensity="dense"
+            label="Controlled picker"
+            value={currentDaySelected}
+            onChange={(newValue) => setCurrentDay(newValue)}
+          />
+        </LocalizationProvider>
+      </Box>
       <Grid sx={{ backgroundColor: '' }} container direction="column" spacing={2}>
         <Grid container item direction="row" justifyContent="space-evenly" alignItems="center">
           <Grid item xs={2} lg={2.5}>
@@ -125,26 +140,28 @@ export default function Analytics() {
           alignItems="center"
           className={styles.info}
         >
-          <BarChart
-            dataset={data2}
-            yAxis={[{ scaleType: 'band', dataKey: 'label' }]}
-            series={[{ dataKey: 'value', valueFormatter }]}
-            layout="horizontal"
-            {...chartSetting}
-            colors={[
-              '#FFA958',
-              '#F48178',
-              '#C96D91',
-              '#8B6595',
-              '#505A7E',
-              '#2F4858',
-              '#549142',
-              '#008151',
-              '#006F5F',
-              '#532F26',
-              '#00D7BD',
-            ]}
-          />
+          {data2.length > 0 ? (
+            <BarChart
+              dataset={data2}
+              yAxis={[{ scaleType: 'band', dataKey: 'label' }]}
+              series={[{ dataKey: 'value', valueFormatter }]}
+              layout="horizontal"
+              {...chartSetting}
+              colors={[
+                '#FFA958',
+                '#F48178',
+                '#C96D91',
+                '#8B6595',
+                '#505A7E',
+                '#2F4858',
+                '#549142',
+                '#008151',
+                '#006F5F',
+                '#532F26',
+                '#00D7BD',
+              ]}
+            />
+          ) : null}
         </Grid>
       </Grid>
     </Container>
